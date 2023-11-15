aefb922ce374a478238c529d962d078d9ac09e85
GraphexDigital <150844895+GraphexDigital@users.noreply.github.com>
2023-11-15 15:50:59 -0400

    Se agregaron los recursos necesarios, se agrego Tailwindcss y Next.UI

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const { nextui } = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: [
+    // ...
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()]
+}
